38c8fa3f920873479fa269263ecffc99edfa1a0d
Zhang Yunchi <zhangyunchi19@mails.ucas.ac.cn>
2021-09-13 07:57:56 +0000

    change runqemu file loc

--- UCAS_OS/Project0-Preparation/run_qemu_p0.sh
+++ UCAS_OS/Project0-Preparation/run_qemu_p0.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 IMG_PATH=test
-/home/stu/OSLab-RISC-V/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel /home/stu/OSLab-RISC-V/project0/add -s -S
+/home/stu/OSLab-RISC-V/qemu-4.1.1/riscv64-softmmu/qemu-system-riscv64 -nographic -machine virt -m 256M -kernel /home/stu/OSLab-RISC-V/oslab/UCAS_OS/Project0-Preparation/add -s -S
